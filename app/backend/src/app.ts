2c39ff4325a57b30f101c0e4d6f3b84b8ad0fb1d
Filipe <filipef.paulo@gmail.com>
2022-06-02 23:31:39 -0300

    Adição das configurações iniciais, como express.json, cors e rotas (index e ErrorMiddleware)

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,30 +1,41 @@
 import * as express from 'express';
+import * as cors from 'cors';
+import ErrorMiddleware from './middlewares/ErrorMiddleware';
+import Routes from './routes';
+import 'express-async-errors';
 
 class App {
   public app: express.Express;
-  // ...
+  private routes: Routes;
 
   constructor() {
-    // ...
+    this.app = express();
+    this.routes = new Routes();
+
     this.config();
-    // ...
   }
 
-  private config():void {
+  private config(): void {
     const accessControl: express.RequestHandler = (_req, res, next) => {
       res.header('Access-Control-Allow-Origin', '*');
-      res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
+      res.header(
+        'Access-Control-Allow-Methods',
+        'GET,POST,DELETE,OPTIONS,PUT,PATCH',
+      );
       res.header('Access-Control-Allow-Headers', '*');
       next();
     };
 
-    this.app.use(accessControl);
-    // ...
+    this.app.use(accessControl).use(express.json()).use(cors());
   }
 
-  // ...
-  public start(PORT: string | number):void {
-    // ...
+  public start(PORT: string | number): void {
+    const { app } = this;
+
+    app.use(this.routes.init());
+    app.use(ErrorMiddleware);
+
+    app.listen(PORT, () => console.log(`Listen on ${PORT}`));
   }
 }
 
