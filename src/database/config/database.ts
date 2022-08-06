b9a5039066d7a531f0ac03579dbbafafc1765ba9
filipe <filipef.paulo@gmail.com>
2022-08-06 00:03:28 -0300

    Restore API

--- src/database/config/database.ts
+++ src/database/config/database.ts
@@ -3,7 +3,7 @@ import { Options } from 'sequelize';
 
 const config: Options = {
   username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || '123456',
+  password: process.env.DB_PASS || 'root',
   database: process.env.DB_NAME || 'TRYBE_FUTEBOL_CLUBE',
   host: process.env.DB_HOST || 'localhost',
   port: Number(process.env.DB_PORT) || 3002,
@@ -12,6 +12,6 @@ const config: Options = {
     timezone: 'Z',
   },
   logging: false,
-}
+};
 
 module.exports = config;
